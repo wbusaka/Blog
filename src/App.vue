6f08c494668240b4da66b0a40e6c17efcf256a79
wbusaka <wilbusaka@gmail.com>
2020-06-14 00:24:00 +0300

    full commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <showBlogs></showBlogs>
+  </div>
+</template>
+
+<script>
+import addBlog from './components/addBlog'
+import showBlogs from './components/showBlogs'
+
+export default {
+
+  components: {
+    'addBlog': addBlog,
+    'showBlogs': showBlogs
+  },
+
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+  body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
+  }
+</style>
